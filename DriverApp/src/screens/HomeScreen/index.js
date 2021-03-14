d980b6b8c417a66b49f28931d1fbcaa2115b2ca4
Enrique <sotomaque@gmail.com>
2021-03-14 14:31:47 -0700

    installed react native maps direction

--- DriverApp/src/screens/HomeScreen/index.js
+++ DriverApp/src/screens/HomeScreen/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {View, Dimensions} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCc2S9XzrUb4Xtz1sGGGbgZWe-m-qcNZzU';
+
 const HomeScreen = () => {
+  const origin = {latitude: 37.3318456, longitude: -122.0296002};
+  const destination = {latitude: 37.771707, longitude: -122.4053769};
   return (
     <View>
       <MapView
@@ -16,8 +21,13 @@ const HomeScreen = () => {
           longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
-        }}
-      />
+        }}>
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+        />
+      </MapView>
     </View>
   );
 };
