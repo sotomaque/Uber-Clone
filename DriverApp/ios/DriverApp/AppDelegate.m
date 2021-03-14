27379888e08eef499cfad9a4ad77281d48100490
Enrique <sotomaque@gmail.com>
2021-03-14 14:20:05 -0700

    google maps integration WIP

--- DriverApp/ios/DriverApp/AppDelegate.m
+++ DriverApp/ios/DriverApp/AppDelegate.m
@@ -4,6 +4,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -27,6 +29,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCc2S9XzrUb4Xtz1sGGGbgZWe-m-qcNZzU"]; // add this line using the api key obtained from Google Console
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
